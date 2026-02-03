4201a43b426b0f0c0f97cf8f6c1ea36fe7d6d18a
Divyanshu Rajput <divyanshurajput709@gmail.com>
2026-02-03 21:36:52 +0530

    chore: explain WS_URL config

--- apps/excelidraw-frontend/config.ts
+++ apps/excelidraw-frontend/config.ts
@@ -1,3 +1,4 @@
 export const HTTP_BACKEND = process.env.NEXT_PUBLIC_HTTP_BACKEND || "http://localhost:3001"
 
+// Use NEXT_PUBLIC_WS_URL as the primary variable for production WebSockets
 export const WS_URL = process.env.NEXT_PUBLIC_WS_URL || process.env.NEXT_PUBLIC_WS_BACKEND || "ws://localhost:8080"
