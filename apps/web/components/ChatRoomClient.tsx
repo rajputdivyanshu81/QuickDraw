0eae17dede2e59036699f6d02c884df314818c3c
Divyanshu Rajput <divyanshurajput709@gmail.com>
2026-05-11 08:32:33 +0530

    fix: resolve lint warnings and disable prop-types in TS

--- apps/web/components/ChatRoomClient.tsx
+++ apps/web/components/ChatRoomClient.tsx
@@ -33,7 +33,7 @@ export const ChatRoomClient: React.FC<{
     }, [socket, loading, id])
 
     return <div>
-        {chats.map(m => <div>{m.message}</div>)}
+        {chats.map((m, index) => <div key={index}>{m.message}</div>)}
 
         <input type="text" value={currentMessage} onChange={e => {
             setCurrentMessage(e.target.value);
